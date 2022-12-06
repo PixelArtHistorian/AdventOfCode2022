3789e043e1e9eafcbd941cc10cf08b68e5d7f74a
A.Diana <a.diana.dev@gmail.com>
2022-12-06 19:28:08 +0100

    'AddedInputsToProjects'

---
+++ AdventOfCode2022/Day6/Program.cs
@@ -0,0 +1,32 @@
+﻿SolvePuzzle1();
+
+
+static void SolvePuzzle1()
+{
+    Queue<char> marker = new Queue<char>();
+
+    string buffer = File.ReadAllText("C:\\Users\\Alberto\\Desktop\\Development\\AdventOfCode2022\\Input\\Day6.txt");
+
+    foreach(char character in buffer)
+    {
+        if (!marker.Contains(character))
+        {
+            marker.Enqueue(character);
+            if(marker.Count == 4)
+            {
+                Console.WriteLine($"Found marker {buffer.IndexOf(character) + 1}");
+            }
+        }
+        else
+        {
+            char repeatedCharacter;
+            do
+            {
+                repeatedCharacter = marker.Dequeue();
+            } 
+            while (repeatedCharacter != character);
+        }
+    }
+
+
+}
